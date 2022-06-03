88d528099334d6bcfeb8e19253648826aa3ce775
Kimkyu <woojoo421@gmail.com>
2022-06-03 15:20:31 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kimkyu:1234@kyuplate.t2pkt.mongodb.net/?retryWrites=true&w=majority', {
+const req = require('express/lib/request')
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!~~'))
+app.get('/', (req, res) => res.send('Hello World!~~새해복많이'))
+
+app.post('/register', (req, res) => {
+    
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
